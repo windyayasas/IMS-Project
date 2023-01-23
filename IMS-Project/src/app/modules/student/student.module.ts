53f3907dd42d96deb8d69856ceb40c5dbf8ed557
KavindaRW <116443764+KavindaRW@users.noreply.github.com>
2023-01-23 12:10:58 +0530

    Achievement page

--- IMS-Project/src/app/modules/student/student.module.ts
+++ IMS-Project/src/app/modules/student/student.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
+import { AchievementsComponent } from './components/achievements/achievements.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AchievementsComponent
+  ],
   imports: [
     CommonModule,
     StudentRoutingModule
