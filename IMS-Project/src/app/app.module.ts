76a9e5f020423ff9e2e2d4d638b77c62e1b0f9ae
KavindaRW <116443764+KavindaRW@users.noreply.github.com>
2023-02-16 09:59:05 +0530

    new code

--- IMS-Project/src/app/app.module.ts
+++ IMS-Project/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MarksComponent } from './marks/marks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarksComponent
   ],
   imports: [
     BrowserModule,
--- IMS-Project/src/app/modules/student/student-routing.module.ts
+++ IMS-Project/src/app/modules/student/student-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { StudentComponent } from './pages/student/student.component';
+import { StudentComponent } from './pages/student/student.marks';
 
 const routes: Routes = [
   {
